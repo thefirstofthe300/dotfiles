32a4dc8d429bc192dc31bfad7b01960b6868f8e4
Danny Seymour <dseymour@taos.com>
2020-06-22 15:11:41 -0700

    feat: update install script

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 sudo apt-get update -y && sudo apt-get install -y zsh
 git submodule update --init --recursive
+if [ -f "${HOME}/.zshrc" ]; then
+  rm "${HOME}/.zshrc"
+fi
 ln -s "$(pwd)/.zshrc" "${HOME}/.zshrc"
+if [ -f "${HOME}/.p10k.zsh" ]; then
+  rm "${HOME}/.p10k.zsh"
+fi
 ln -s "$(pwd)/p10k.zsh" "${HOME}/.p10k.zsh"
 
